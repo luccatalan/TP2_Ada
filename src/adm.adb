cef540cf1ff76e16df0fc8d1f014f895cc76a99a
Catalan Luc <luc.catalan@gmail.com>
2018-11-10 15:38:08 +0100

    Calcul altitude

--- src/adm.adb
+++ src/adm.adb
@@ -1,7 +1,9 @@
 with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Numerics.Elementary_Functions;
+use Ada.Numerics.Elementary_Functions;
 
 package body ADM is
-
+      
    procedure update(This : access T_ADM; sensor : access T_MeasureDevice) is
       p : Float;
    begin
@@ -19,8 +21,27 @@ package body ADM is
    end;
    
    procedure computeAltitude(This : in out T_ADM) is
+      i : Integer := 0;
+      g : Float := 9.807;
+      p0 : Float := 1013.15;
+      R : Float := 8.314;
+      M : Float := 0.02896;
+      T0 : Float := 288.0;
+      h : Float := 0.0;
    begin
-      null;
+      -- Calcul pression moyenne
+      This.pressureMean := 0.0;
+      This.status := False;
+      while i < Integer(This.listOfPressure.Length) loop 
+         if This.listOfPressure.Element(i) /= -1.0 then 
+            This.pressureMean := This.pressureMean + This.listOfPressure.Element(i);
+            This.status := True;
+         end if;
+         i := i + 1;
+      end loop;
+      This.pressureMean := This.pressureMean / Float(This.listOfPressure.Length);
+      -- Calcul altitude
+      h := ((R * T0) / (M * g)) * Log(p0 / This.pressureMean);
    end;
 
 end ADM;
--- src/object.ads
+++
@@ -1,5 +0,0 @@
-package Object is
-
-   type T_Object is abstract tagged null record;
-
-end Object;
